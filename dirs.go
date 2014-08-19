3c379cf101f1980b1eb36e6ed8eac857766e31d2
Arunjit Singh <opensrc@ajsd.in>
2014-08-20 00:12:18 +0530

    cleanup and add CORS

---
+++ dirs.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path"
+	"strings"
+)
+
+var (
+	baseFlag = flag.String("base", "", "Absolute path to the base directory.")
+)
+
+type FileInfo struct {
+	IsDir bool   `json:"is_dir"`
+	Name  string `json:"name"`
+	Path  string `json:"path"`
+	Size  int64  `json:"size"`
+}
+
+func skip(name string) bool {
+	return strings.HasPrefix(name, ".")
+}
+
+func ListFilesHandler(w http.ResponseWriter, r *http.Request) {
+	p := path.Join(*baseFlag, r.URL.Path)
+	if !strings.HasPrefix(p, *baseFlag) {
+		http.Error(w, "Bad path", http.StatusForbidden)
+		return
+	}
+	infos, err := ioutil.ReadDir(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var ret []FileInfo
+	for _, info := range infos {
+		if skip(info.Name()) {
+			continue
+		}
+		finfo := FileInfo{
+			IsDir: info.IsDir(),
+			Name:  info.Name(),
+			Path:  path.Join(r.URL.Path, info.Name()),
+			Size:  info.Size(),
+		}
+		ret = append(ret, finfo)
+	}
+	if err := json.NewEncoder(w).Encode(ret); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	}
+}
+
+func init() {
+	flag.Parse()
+	if *baseFlag == "" {
+		log.Fatalf("-base is required [%s]\n", *baseFlag)
+	}
+	if !strings.HasPrefix(*baseFlag, "/") {
+		log.Fatalln("-base must be absolute")
+	}
+}
---
+++ whitelist.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"flag"
+	"log"
+	"os"
+	"strings"
+)
+
+var whitelist *list.List
+
+var (
+	whitelistFileFlag = flag.String("cors-whitelist", "", "File containing allowed CORS origins")
+)
+
+func IsWhitelisted(origin string) bool {
+	if whitelist == nil {
+		return false
+	}
+	for e := whitelist.Front(); e != nil; e = e.Next() {
+		if e.Value == origin {
+			return true
+		}
+	}
+	return false
+}
+
+func readWhitelistFile(name string) error {
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	newlist := list.New()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") || strings.HasPrefix(line, "//") {
+			continue
+		}
+		newlist.PushBack(line)
+	}
+	whitelist.Init()
+	whitelist.PushBackList(newlist)
+	for e := whitelist.Front(); e != nil; e = e.Next() {
+		if origin, ok := e.Value.(string); ok {
+			log.Printf("Adding %s to whitelist", origin)
+		}
+	}
+	return nil
+}
+
+func GetWhitelist() []string {
+	if whitelist == nil {
+		return nil
+	}
+	w := make([]string, whitelist.Len())
+	for e := whitelist.Front(); e != nil; e = e.Next() {
+		if origin, ok := e.Value.(string); ok {
+			w = append(w, origin)
+		}
+	}
+	return w
+}
+
+func init() {
+	flag.Parse()
+	if *whitelistFileFlag == "" {
+		log.Printf("No whitelist specified for CORS [%s]. Cross-origin requests will have default behaviour.\n",
+			*whitelistFileFlag)
+		whitelist = nil
+		return
+	}
+	whitelist = list.New()
+	readWhitelistFile(*whitelistFileFlag)
+}
